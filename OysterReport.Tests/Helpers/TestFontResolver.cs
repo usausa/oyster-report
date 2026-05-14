d1c046ce73774430c62717f47b9e65b659eb347a
usausa <machi.pon@gmail.com>
2026-05-14 22:13:00 +0900

    Update

--- OysterReport.Tests/Helpers/TestFontResolver.cs
+++ OysterReport.Tests/Helpers/TestFontResolver.cs
@@ -4,19 +4,20 @@ internal sealed class TestFontResolver : IReportFontResolver
 {
     private const string EmbeddedFontName = "IPAexGothic";
 
-    private static readonly HashSet<string> GothicFontNames =
-        new(StringComparer.OrdinalIgnoreCase)
-        {
-            "ＭＳ Ｐゴシック",
-            "MS Pゴシック",
-            "ＭＳ ゴシック",
-            "メイリオ",
-            "Meiryo",
-            "游ゴシック",
-            "Yu Gothic",
-            "游ゴシック Medium",
-            "Yu Gothic Medium"
-        };
+#pragma warning disable IDE0028
+    private static readonly HashSet<string> GothicFontNames = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "ＭＳ Ｐゴシック",
+        "MS Pゴシック",
+        "ＭＳ ゴシック",
+        "メイリオ",
+        "Meiryo",
+        "游ゴシック",
+        "Yu Gothic",
+        "游ゴシック Medium",
+        "Yu Gothic Medium"
+    };
+#pragma warning restore IDE0028
 
     private readonly ReadOnlyMemory<byte> fontData;
 
